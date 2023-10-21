83a3b2b5f4110c3eb84f335efadc73d0b2188d45
krizh-p <kpurohit@gmu.edu>
2023-10-21 17:43:58 -0400

    sticky notes are now persistent

--- js/menu.js
+++ js/menu.js
@@ -16,6 +16,44 @@ function toggleBar(size) {
     pomodoroIcon.style.width = noteIcon.style.width = clockIcon.style.width = timeBar.style.height = size;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+    for (let noteID in existingNotes) {
+        var note = existingNotes[noteID];
+        loadStickyNote(noteID, note.text, note.coords)
+    }
+})
+
+function loadStickyNote(noteID, text, coords) {
+    // Create a new sticky note element
+    const stickyNote = document.createElement('div');
+    stickyNote.classList.add('stickynote');
+    makeRightDraggable(stickyNote);
+    makeEditable(stickyNote);
+
+    const span = document.createElement('span');
+    span.textContent = text;
+    stickyNote.appendChild(span);
+
+    // Set the coordinates for the sticky note
+    // stickyNote.style.left = coords[0] + 'px';
+    // stickyNote.style.top = coords[1] + 'px';
+
+    const maxWidth = document.documentElement.clientWidth - 160;
+    const maxHeight = document.documentElement.clientHeight - 160;
+
+    // Ensure the sticky note stays within the viewport bounds
+    stickyNote.style.left = Math.min(coords[0], maxWidth) + 'px';
+    stickyNote.style.top = Math.min(coords[1], maxHeight) + 'px';
+
+    // Add the ID as a data attribute to the sticky note
+    stickyNote.dataset.noteId = noteID;
+
+    // Append the sticky note to the document
+    document.body.appendChild(stickyNote);
+}
+
+
 function createStickyNote() {
     const x = 100; // Adjust the initial position as needed
     const y = 100; // Adjust the initial position as needed
@@ -36,13 +74,34 @@ function createStickyNote() {
     stickyNote.style.left = Math.min(x, maxWidth) + 'px';
     stickyNote.style.top = Math.min(y, maxHeight) + 'px';
 
+    // Define the note object
+    newNoteID = (localStorage.getItem('notesList') == null) ? ((1 ^ Date.now()) % 94) : ((localStorage.getItem('notesList').length ^ Date.now()) % 94);
+
+    var note = {
+        ID: newNoteID,
+        text: span.textContent,
+        coords: [x, y]
+    };
+
+    // Get the existing notes from local storage or initialize an empty array
+    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+
+    // Add the new note to the array
+    existingNotes[newNoteID] = note;
+
+    // Store the updated notes list in local storage
+    localStorage.setItem('notesList', JSON.stringify(existingNotes));
+
+    stickyNote.dataset.noteId = newNoteID;
     document.body.appendChild(stickyNote);
+    
 }
 
 function makeRightDraggable(element) {
     let isDragging = false;
 
     element.addEventListener('mousedown', (e) => {
+        noteID = element.dataset.noteId;
         if (e.button === 2) { // Right-click button is clicked
             isDragging = true;
             const offsetX = e.clientX - element.getBoundingClientRect().left;
@@ -66,7 +125,12 @@ function makeRightDraggable(element) {
 
                 // Re-enable text selection after dragging
                 element.style.userSelect = 'auto';
-
+                
+                // Update element x, y
+                var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+                existingNotes[noteID].coords = [parseInt(element.style.left), parseInt(element.style.top)]
+                localStorage.setItem('notesList', JSON.stringify(existingNotes));
+            
                 document.removeEventListener('mousemove', onMouseMove);
                 document.removeEventListener('mouseup', onMouseUp);
             }
@@ -87,6 +151,12 @@ function makeEditable(element) {
         if (isEditing) {
             element.contentEditable = 'false';
             isEditing = false;
+
+            // update text
+            var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+            existingNotes[noteID].text = element.textContent;
+            localStorage.setItem('notesList', JSON.stringify(existingNotes));
+            console.log('Updated text content for ${noteID}}')
         }
     });
 
