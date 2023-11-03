c43be69c02e31bdc80974e384c04c3e2c59905d7
krizh-p <kpurohit@gmu.edu>
2023-11-03 10:30:20 -0400

    implemented sticky notes

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,14 @@
 var menuOn = false;
 toggleBar('0em');
 
+var zIndexCounter = (localStorage.getItem('zIndexCounter')) ? localStorage.getItem('zIndexCounter') : 0;
+localStorage.setItem('zIndexCounter', zIndexCounter);
+
+const md = window.markdownit();
+
+/**
+ * Animate clicking the menu icon
+ */
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('active');
     menuOn = !menuOn;
@@ -10,160 +18,6 @@ menuIcon.addEventListener('click', () => {
         toggleBar('0em');
 });
 
-noteIcon.addEventListener('click', createStickyNote);
-
 function toggleBar(size) {
     pomodoroIcon.style.width = noteIcon.style.width = clockIcon.style.width = timeBar.style.height = size;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
-    for (let noteID in existingNotes) {
-        var note = existingNotes[noteID];
-        loadStickyNote(noteID, note.text, note.coords)
-    }
-})
-
-function loadStickyNote(noteID, text, coords) {
-    // Create a new sticky note element
-    const stickyNote = document.createElement('div');
-    stickyNote.classList.add('stickynote');
-    makeRightDraggable(stickyNote);
-    makeEditable(stickyNote);
-
-    const span = document.createElement('span');
-    span.textContent = text;
-    stickyNote.appendChild(span);
-
-    // Set the coordinates for the sticky note
-    // stickyNote.style.left = coords[0] + 'px';
-    // stickyNote.style.top = coords[1] + 'px';
-
-    const maxWidth = document.documentElement.clientWidth - 160;
-    const maxHeight = document.documentElement.clientHeight - 160;
-
-    // Ensure the sticky note stays within the viewport bounds
-    stickyNote.style.left = Math.min(coords[0], maxWidth) + 'px';
-    stickyNote.style.top = Math.min(coords[1], maxHeight) + 'px';
-
-    // Add the ID as a data attribute to the sticky note
-    stickyNote.dataset.noteId = noteID;
-
-    // Append the sticky note to the document
-    document.body.appendChild(stickyNote);
-}
-
-
-function createStickyNote() {
-    const x = 100; // Adjust the initial position as needed
-    const y = 100; // Adjust the initial position as needed
-
-    const stickyNote = document.createElement('div'); // Use a div for content editable
-    stickyNote.classList.add('stickynote');
-    makeRightDraggable(stickyNote);
-    makeEditable(stickyNote);
-
-    const span = document.createElement('span');
-    span.textContent = 'This is a sticky note!';
-    stickyNote.appendChild(span);
-
-    const maxWidth = document.documentElement.clientWidth - 160;
-    const maxHeight = document.documentElement.clientHeight - 160;
-
-    // Ensure the sticky note stays within the viewport bounds
-    stickyNote.style.left = Math.min(x, maxWidth) + 'px';
-    stickyNote.style.top = Math.min(y, maxHeight) + 'px';
-
-    // Define the note object
-    newNoteID = (localStorage.getItem('notesList') == null) ? ((1 ^ Date.now()) % 94) : ((localStorage.getItem('notesList').length ^ Date.now()) % 94);
-
-    var note = {
-        ID: newNoteID,
-        text: span.textContent,
-        coords: [x, y]
-    };
-
-    // Get the existing notes from local storage or initialize an empty array
-    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
-
-    // Add the new note to the array
-    existingNotes[newNoteID] = note;
-
-    // Store the updated notes list in local storage
-    localStorage.setItem('notesList', JSON.stringify(existingNotes));
-
-    stickyNote.dataset.noteId = newNoteID;
-    document.body.appendChild(stickyNote);
-    
-}
-
-function makeRightDraggable(element) {
-    let isDragging = false;
-
-    element.addEventListener('mousedown', (e) => {
-        noteID = element.dataset.noteId;
-        if (e.button === 2) { // Right-click button is clicked
-            isDragging = true;
-            const offsetX = e.clientX - element.getBoundingClientRect().left;
-            const offsetY = e.clientY - element.getBoundingClientRect().top;
-
-            // Prevent text selection during drag
-            element.style.userSelect = 'none';
-
-            document.addEventListener('mousemove', onMouseMove);
-            document.addEventListener('mouseup', onMouseUp);
-
-            function onMouseMove(e) {
-                if (isDragging) {
-                    element.style.left = e.clientX - offsetX + 'px';
-                    element.style.top = e.clientY - offsetY + 'px';
-                }
-            }
-
-            function onMouseUp() {
-                isDragging = false;
-
-                // Re-enable text selection after dragging
-                element.style.userSelect = 'auto';
-                
-                // Update element x, y
-                var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
-                existingNotes[noteID].coords = [parseInt(element.style.left), parseInt(element.style.top)]
-                localStorage.setItem('notesList', JSON.stringify(existingNotes));
-            
-                document.removeEventListener('mousemove', onMouseMove);
-                document.removeEventListener('mouseup', onMouseUp);
-            }
-        }
-    });
-}
-
-function makeEditable(element) {
-    let isEditing = false;
-
-    element.addEventListener('dblclick', () => {
-        element.contentEditable = 'true';
-        element.focus();
-        isEditing = true;
-    });
-
-    element.addEventListener('blur', () => {
-        if (isEditing) {
-            element.contentEditable = 'false';
-            isEditing = false;
-
-            // update text
-            var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
-            existingNotes[noteID].text = element.textContent;
-            localStorage.setItem('notesList', JSON.stringify(existingNotes));
-            console.log('Updated text content for ${noteID}}')
-        }
-    });
-
-    element.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter' && !event.shiftKey) {
-            element.contentEditable = 'false';
-            event.preventDefault();
-        }
-    });
 }
---
+++ js/notes.js
@@ -0,0 +1,164 @@
+var zIndexCounter = (localStorage.getItem('zIndexCounter')) ? localStorage.getItem('zIndexCounter') : 0;
+localStorage.setItem('zIndexCounter', zIndexCounter);
+
+// Create a sticky note when you click on the sticky note icon
+noteIcon.addEventListener('click', loadStickyNote);
+
+// Load existing sticky notes when page is opened
+document.addEventListener("DOMContentLoaded", () => {
+    
+    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+    for (let noteID in existingNotes) {
+        var note = existingNotes[noteID];
+        let loadedNote = loadStickyNote(noteID, note.text, note.coords, note.size);
+        loadedNote.innerHTML = md.render(note.text);
+        // loadedNote.querySelector('textarea').style.display = "none";
+        if (!(loadedNote.querySelector('textarea'))) {
+            var textArea = document.createElement("textarea");
+            textArea.value = note.text;
+            loadedNote.appendChild(textArea);
+        }
+        addMarkdown(loadedNote, textArea);
+        loadedNote.querySelector('textarea').style.display = "none";
+    }
+})
+
+function createNote(noteID, text, coords, size) {
+    var textArea = document.createElement("textarea");
+    
+    const note = document.createElement("div");
+    note.className = "sticky";
+    var xCord = Math.random() * (window.innerWidth - 500);
+    var yCord = Math.random() * (window.innerHeight - 500);
+    note.style.left = xCord + "px";
+    note.style.top = yCord + "px";
+    note.style.zIndex = zIndexCounter++;
+    if (noteID)
+        note.dataset.noteID = noteID;
+    else
+        note.dataset.noteID = (localStorage.getItem('notesList') == null) ? ((1 ^ Date.now()) % 999) : ((localStorage.getItem('notesList').length ^ Date.now()) % 999);
+
+    if (size) {
+        note.style.width = size[0] + "px";
+        note.style.height = size[1] + "px";
+    }
+
+    if (coords) {
+        note.style.left = coords[0] + "px";
+        note.style.top = coords[1] + "px";
+    }
+
+    if (text)
+        textArea.value = text;
+
+    note.appendChild(textArea);
+
+    return note;
+}
+
+function addDragAndResize(note) {
+    let isDragging = false;
+    let isResizing = false;
+    let offsetX, offsetY;
+    let startX, startY;
+    let initialWidth, initialHeight;
+
+    note.addEventListener("mousedown", (e) => {
+        if (e.button === 0) { // Left mouse button
+            isDragging = true;
+            offsetX = e.clientX - note.getBoundingClientRect().left;
+            offsetY = e.clientY - note.getBoundingClientRect().top;
+        } else if (e.button === 2) { // Right mouse button
+            isResizing = true;
+            initialWidth = note.clientWidth;
+            initialHeight = note.clientHeight;
+            startX = e.clientX;
+            startY = e.clientY;
+        }
+    });
+
+    document.addEventListener("mousemove", (e) => {
+        if (isDragging) {
+            note.style.left = e.clientX - offsetX + "px";
+            note.style.top = e.clientY - offsetY + "px";
+
+            // update local storage
+            const notesList = JSON.parse(localStorage.getItem('notesList')) || {};
+            notesList[note.dataset.noteID].coords = [parseInt(note.style.left), parseInt(note.style.top)];
+            localStorage.setItem('notesList', JSON.stringify(notesList));
+        }
+        if (isResizing) {
+            const newWidth = initialWidth + (e.clientX - startX);
+            const newHeight = initialHeight + (e.clientY - startY);
+            note.style.width = newWidth + "px";
+            note.style.height = newHeight + "px";
+            
+            // update local storage
+            const notesList = JSON.parse(localStorage.getItem('notesList')) || {};
+            notesList[note.dataset.noteID].size = [parseInt(note.style.width), parseInt(note.style.height)];
+            localStorage.setItem('notesList', JSON.stringify(notesList));
+            
+        }
+    });
+
+    document.addEventListener("mouseup", () => {
+        isDragging = false;
+        isResizing = false;
+    });
+}
+
+function addMarkdown(note, textArea) {
+    // Handle Markdown rendering when the textarea loses focus
+    textArea.addEventListener("blur", () => {
+        const markdownText = textArea.value;
+        note.innerHTML = md.render(markdownText);
+        note.appendChild(textArea);
+        textArea.style.display = "none";
+
+        // update local storage
+        const notesList = JSON.parse(localStorage.getItem('notesList')) || {};
+        notesList[note.dataset.noteID].text = textArea.value
+        localStorage.setItem('notesList', JSON.stringify(notesList));
+    });
+
+    note.addEventListener("dblclick", () => {
+        textArea.style.display = "block";
+        textArea.focus();
+    });
+}
+
+function loadStickyNote(noteID, text, coords, size) {
+    var note;
+    var textArea;
+    if (noteID && text && coords && size) {
+        note = createNote(noteID, text, coords, size);
+    }
+    else
+        note = createNote();
+    textArea = note.querySelector('textarea');
+    addDragAndResize(note);
+
+    addMarkdown(note, textArea);
+    
+
+
+    var noteObj = {
+        ID: note.dataset.noteID,
+        text: textArea.value,
+        coords: [parseInt(note.style.left), parseInt(note.style.top)],
+        size: [parseInt(note.style.width), parseInt(note.style.height)]
+    };
+
+    // Get the existing notes from local storage or initialize an empty array
+    var existingNotes = JSON.parse(localStorage.getItem('notesList')) || {};
+
+    // Add the new note to the array
+    existingNotes[note.dataset.noteID] = noteObj;
+
+    // Store the updated notes list in local storage
+    localStorage.setItem('notesList', JSON.stringify(existingNotes));
+    document.body.appendChild(note);
+    return note;
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
